80380622186b0ff84d835817015a9c7584160aa7
Kristen Hazard <kristen@hazardbio.com>
2012-01-21 11:27:35 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cosmicshelf::Application.config.secret_token = 'eeb4791c3db16c5d40a21254fb053d479455841f51c9f28fa23e308c30d2a3d8b0bd1b5c5b4a380c7acd1ff06d56425b6f8678fcd4f14a8eb089adb0248f1c4d'
